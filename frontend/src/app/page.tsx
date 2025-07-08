a31591e9e37a7cf654642b6fedbe3ed4f11ec127
shotaro <850123.a@gmail.com>
2025-07-08 16:57:38 +0900

    期間とチャンネルID入力で履歴取得実装

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,30 +1,68 @@
 "use client";
 
-const SLACK_JISOU_TWEET_CHANNEL_ID = "C06EH0P13MW";
-const OLDEST = new Date("2025-07-06T00:00:00+09:00").getTime()/1000;
-console.log(OLDEST); // 1751727600000
-console.log(Intl.DateTimeFormat().resolvedOptions().timeZone);
+import { useState } from "react";
 
-const LATEST = new Date("2025-07-07T00:00:00+09:00").getTime()/1000;
+const SLACK_JISOU_TWEET_CHANNEL_ID = "C06EH0P13MW";
 
 export default function Home() {
+  const [oldestDate, setOldestDate] = useState("");
+  const [latestDate, setLatestDate] = useState("");
+  const [channelId, setChannelId] = useState("");
+
+  const UnixTimestampFarmattedOldestDate = new Date(`${oldestDate}T00:00:00+09:00`).getTime() / 1000;
+
+  const UnixtimestampFarmattedLatestDate = new Date(`${latestDate}T00:00:00+09:00`).getTime() / 1000;
+
   const handleSlackApi = async () => {
     try {
-      const res = await fetch(`/api/slack-history?channel=${SLACK_JISOU_TWEET_CHANNEL_ID}&oldest=${OLDEST}&latest=${LATEST}`);
-      if (!res.ok) { // responseは返ってきているが200以外
+      const res = await fetch(
+        `/api/slack-history?channel=${channelId}&oldest=${UnixTimestampFarmattedOldestDate}&latest=${UnixtimestampFarmattedLatestDate}`
+      );
+      if (!res.ok) {
+        // responseは返ってきているが200以外
         const errData = await res.json();
         console.log("API Error", errData);
         return;
       }
       const data = await res.json();
       console.log("data", data);
-    } catch (error) { // ﾈｯﾄﾜｰｸｴﾗｰやﾀｲﾑｱｳﾄなどﾚｽﾎﾟﾝｽが返ってこない時catchに入る
+    } catch (error) {
+      // ﾈｯﾄﾜｰｸｴﾗｰやﾀｲﾑｱｳﾄなどﾚｽﾎﾟﾝｽが返ってこない時catchに入る
       console.log("Fetch Error", error);
     }
   };
   return (
     <div>
-      <button className="border" onClick={handleSlackApi}>
+      <div>
+        <label>チャンネルID</label>C06EH0P13MW
+        <input
+          className="border rounded-md p-1"
+          placeholder="チャンネルIDを入力"
+          type="text"
+          value={channelId}
+          onChange={(e) => setChannelId(e.target.value)}
+         />
+      </div>
+      <div>
+        <label>いつから</label>
+        <input
+          className="border rounded-md"
+          type="date"
+          value={oldestDate}
+          onChange={(e) => setOldestDate(e.target.value)}
+         />
+      </div>
+      <div>
+        <label>いつまで</label>
+        <input
+          className="border rounded-md"
+          type="date"
+          value={latestDate}
+          onChange={(e) => setLatestDate(e.target.value)}
+        />
+      </div>
+
+      <button className="border rounded-md bg-teal-500" onClick={handleSlackApi}>
         Slackから履歴データ取得
       </button>
     </div>
